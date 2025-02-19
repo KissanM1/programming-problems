075bc1a421cb469208b52ca5828ff9ce7257bd77
Kissan <173943794+KissanM1@users.noreply.github.com>
2025-02-19 10:42:12 -0500

    Solution for Recursion-1 - sumDigits

---
+++ CodingBat/sumDigits.java
@@ -0,0 +1,10 @@
+package CodingBat;
+
+public class sumDigits {
+    public int sumDigits(int n) {
+        if (n < 10){
+          return n;
+        }   
+        return n% 10 + sumDigits(n/10);
+      }      
+}
