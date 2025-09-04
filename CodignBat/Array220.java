c936afcc4b92f680100659cbbcd315a75eb589e5
Kissan <173943794+KissanM1@users.noreply.github.com>
2025-09-04 16:55:47 -0400

    Revert "Add array220 recursion solution"
    This reverts commit e8709bfc1d8d722ac6e78ed683d462c6a97de926.

--- CodignBat/Array220.java
+++
@@ -1,8 +0,0 @@
-public class Array220 {
-    public boolean array220(int[] nums, int index) {
-        if (index + 1 >= nums.length) {
-            return false;
-        }
-        return nums[index] * 10 == nums[index + 1] ? true :  array220(nums, index + 1);
-    }
-}
