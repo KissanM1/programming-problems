c4b3e5165e0d63459509c2db4f4acf710ef36e7a
Kissan <173943794+KissanM1@users.noreply.github.com>
2025-10-06 13:49:57 -0400

    Update CCC 2022 J1 Cupcake Party solution

--- CCC/CupcakeParty.cpp
+++ CCC/CupcakeParty.cpp
@@ -1,3 +1,9 @@
+/*
+A regular box of cupcakes holds 8 cupcakes, while a small box holds 3 cupcakes. There are 28
+students in a class and a total of at least 28 cupcakes. Your job is to determine how many 
+cupcakes will be left over if each student gets one cupcake.
+*/
+
 #include <iostream>
 using namespace std;
 
